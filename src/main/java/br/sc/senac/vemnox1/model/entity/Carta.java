b85ceab455c79abfa29b8e7eb953def55a5798d0
arthurbowens <arthurbowens18@gmail.com>
2024-08-21 18:12:47 -0300

    second commit

---
+++ src/main/java/br/sc/senac/vemnox1/model/entity/Carta.java
@@ -0,0 +1,31 @@
+package br.sc.senac.vemnox1.model.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Carta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private int forca;
+    private int inteligencia;
+    private int velocidade;
+
+    @CreationTimestamp
+    private LocalDate dataCadastro;
+}
